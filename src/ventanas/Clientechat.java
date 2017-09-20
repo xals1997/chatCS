c1632fc01c29845c4e0bdc06e413db232d3fb603
Alberto <alberto.berenguer.97@gmail.com>
2017-09-20 17:51:19 +0200

    primera subida

---
+++ src/ventanas/Clientechat.java
@@ -0,0 +1,12 @@
+
+package ventanas;
+
+/**
+ * @author Alberto Berenguer
+ */
+public class Clientechat {
+    public static void main(String[] args) {
+      InterfazConfiguracion r = new InterfazConfiguracion();
+      r.setVisible(true);
+    } 
+}
